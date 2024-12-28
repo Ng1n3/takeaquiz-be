6f6cf4483b464c1e1f51fae7ffc0c73eb73a3a5c
ng1n3 <code.with.muyiwa@gmail.com>
2024-12-29 00:00:54 +0100

    fix: unique index on usersAnwers schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,8 +3,8 @@ import {
   pgTable,
   text,
   timestamp,
+  uniqueIndex,
   uuid,
-  index
 } from 'npm:drizzle-orm/pg-core';
 
 const timeStamp = {
@@ -18,22 +18,24 @@ const timeStamp = {
 };
 
 export const users = pgTable('users', {
-  id: uuid('id').primaryKey().defaultRandom(),
+  id: uuid('id').primaryKey().notNull(),
   email: text('email').unique().notNull(),
   password: text('password').notNull(),
   ...timeStamp,
 });
 
 export const tests = pgTable('tests', {
-  id: uuid('id').primaryKey().defaultRandom(),
+  id: uuid('id').primaryKey().notNull(),
   title: text('title').notNull(),
   description: text('decription'),
-  createdBy: uuid('created_by').references(() => users.id).notNull(),
+  createdBy: uuid('created_by')
+    .references(() => users.id)
+    .notNull(),
   ...timeStamp,
 });
 
 export const questions = pgTable('questions', {
-  id: uuid('id').primaryKey().defaultRandom(),
+  id: uuid('id').primaryKey().notNull(),
   testId: uuid('test_id')
     .references(() => tests.id)
     .notNull(),
@@ -42,31 +44,42 @@ export const questions = pgTable('questions', {
 });
 
 export const answers = pgTable('answers', {
-  id: uuid('id').primaryKey().defaultRandom(),
+  id: uuid('id').primaryKey().notNull(),
   questionId: uuid('question_id')
     .references(() => questions.id)
     .notNull(),
-  content: text('content').notNull()
+  content: text('content').notNull(),
 });
 
-export const userAnswers = pgTable('user_answers', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  userId: uuid('user_id')
-    .references(() => users.id)
-    .notNull(),
-  questionId: uuid('question_id')
-    .references(() => questions.id)
-    .notNull(),
-  answerId: uuid('answer_id')
-    .references(() => answers.id)
-    .notNull(),
-  ...timestamp,
-})
+export const userAnswers = pgTable(
+  'user_answers',
+  {
+    id: uuid('id').primaryKey().notNull(),
+    userId: uuid('user_id')
+      .references(() => users.id)
+      .notNull(),
+    questionId: uuid('question_id')
+      .references(() => questions.id)
+      .notNull(),
+    answerId: uuid('answer_id')
+      .references(() => answers.id)
+      .notNull(),
+    ...timestamp,
+  },
+  (table) => {
+    return {
+      userAnswerUnique: uniqueIndex('user_answer_unique').on(
+        table.userId,
+        table.questionId
+      ),
+    };
+  }
+);
 
-index('unique_user_answer', ['userId', 'questionId'], {unique: true})
+// index('user_answer_unique', ['userId', 'questionId'], { unique: true });
 
 export const testResults = pgTable('test_results', {
-  id: uuid('id').primaryKey().defaultRandom(),
+  id: uuid('id').primaryKey().notNull(),
   userId: uuid('user_id')
     .references(() => users.id)
     .notNull(),
