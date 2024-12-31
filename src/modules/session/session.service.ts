19ed83ed53f2cc23e141670535014d7d096f6358
ng1n3 <code.with.muyiwa@gmail.com>
2025-01-01 00:05:25 +0100

    feat: create a session

--- src/modules/session/session.service.ts
+++ src/modules/session/session.service.ts
@@ -1,3 +1,18 @@
-async function createSession(userId:string, userAgent: string) {
-  const session = awit 
+import { DB } from '../../db';
+import { sessions } from '../../db/schema';
+import { AuthenticationError } from '../../error/AuthenticationError';
+
+export async function createSession(db: DB, userId: string, userAgent: string) {
+  try {
+    const [session] = await db
+      .insert(sessions)
+      .values({ userId, userAgent, valid: true })
+      .returning();
+
+    return session;
+  } catch (error) {
+    throw new AuthenticationError('Error creating session');
+  }
 }
+
+
